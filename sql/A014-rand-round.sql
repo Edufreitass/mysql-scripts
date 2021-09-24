34924a4de5269dc428e8f9af889028943ebe0c94
Eduardo Freitas <eduardoflorencio96@gmail.com>
2021-09-24 00:05:04 -0300

    Aula 18: RAND e ROUND

---
+++ sql/A014-rand-round.sql
@@ -0,0 +1,17 @@
+-- Configura um salário aleatório para users
+-- SELECT ROUND(RAND() * 10000, 2);
+UPDATE users SET salary = ROUND(RAND() * 10000, 2);
+
+SELECT
+	salary
+FROM
+	users
+WHERE
+	salary 
+BETWEEN 
+	1000 
+AND 
+	1500
+ORDER BY 
+	salary 
+ASC;
